317430372bf8972775c8d20480276dbf5691b3d6
danglaiacc <luubaonlai@gmail.com>
2020-09-18 12:22:57 +0700

    chinh sua database

--- temp/database.sql
+++ temp/database.sql
@@ -11,4 +11,8 @@ go
 create table CauHoi(
 	MaCauHoi char(10) primary key,
 	NoiDung nvarchar(1000),
+	DapAn0 nvarchar(100),
+	DapAn1 nvarchar(100),
+	DapAn2 nvarchar(100),
+	DapAn3 nvarchar(100)
 )
